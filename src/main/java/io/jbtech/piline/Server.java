d0b9304e854c4bf8bbfbb1c9c26b3802bd6179db
jonayed-xlab <jonayed.baperi23@gmail.com>
2024-07-31 16:11:30 +0600

    Add getName method

--- src/main/java/io/jbtech/piline/Server.java
+++ src/main/java/io/jbtech/piline/Server.java
@@ -1,6 +1,7 @@
 package io.jbtech.piline;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class Server {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String getName(@PathVariable("name") String name) {
+        return "Hello ${name}";
     }
 }
